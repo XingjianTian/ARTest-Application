323a50f8e6c4eb566f326f3f3d38268281ce66e0
XingjianTian <793984912@qq.com>
2017-12-03 10:45:42 +0800

    Check

---
+++ Assets/Resources/Focus.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Focus : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		Vuforia.CameraDevice.Instance.SetFocusMode(Vuforia.CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
